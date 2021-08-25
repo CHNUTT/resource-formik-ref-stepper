d53c13f1c6c8fd01ec22422555ad709d3d40286c
CHNUTT <65862558+CHNUTT@users.noreply.github.com>
2021-08-25 12:38:40 -0500

    [ First Step ] - Layout 1 * basic layouts | initialValues * start values for the form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,45 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
+import { Field, Form, Formik } from 'formik';
+import { CheckboxWithLabel, TextField } from 'formik-material-ui';
+import { mixed, number, object } from 'yup';
 
 export default function Home() {
-  return (
-    <Card>
-      <CardContent>
-        <Typography variant="h1">Hello Youtube!!!</Typography>
-      </CardContent>
-    </Card>
-  );
+	return (
+		<Card>
+			<CardContent>
+				<Formik
+					initialValues={{
+						firstName: '',
+						lastName: '',
+						millionaire: false,
+						money: 0,
+						description: '',
+					}}
+					onSubmit={() => {}}
+				>
+					<Form autoComplete='off'>
+						<Field name='firstName' component={TextField} label='First Name' />
+						<Field name='lastName' component={TextField} label='Last Name' />
+						<Field
+							component={CheckboxWithLabel}
+							name='millionaire'
+							type='checkbox'
+							label={{ label: 'I am a millionair' }}
+						/>
+						<Field
+							name='money'
+							type='number'
+							component={TextField}
+							label='All the money I have'
+						/>
+						<Field
+							name='description'
+							component={TextField}
+							label='Description'
+						/>
+					</Form>
+				</Formik>
+			</CardContent>
+		</Card>
+	);
 }
